e70821e983707cee2de87f34bb3439ace5d92f9e
Daniel Nimratz <daniel.nimratz@gmail.com>
2021-11-04 10:06:41 +0200

    little changes in Task 1

--- Lesson#7 Homework/Task 1/first_java_script_file.js
+++ Lesson#7 Homework/Task 1/first_java_script_file.js
@@ -90,3 +90,47 @@ console.log(c4)
 console.log(d4)
 console.log(e4)
 console.log(f4)
+
+console.log("-----")
+console.log("Fourth part")
+console.log("-----")
+
+// 1 + ‘2’; 
+// ‘ ’ + 1 + 0 ;  
+// ‘ ‘- 1 + 0 ; 
+//  ’ ‘2’ * ‘3’;  4
+//  ’ 2 + 2 + ‘px’; 5 
+//  ‘px’ + 2 + 1;  6
+//  ‘2’ - 1; ‘2px’ - 1 7
+//  ;  Null + 1; 8
+// Undefined + 1 9
+console.log(1 + '2')
+console.log(typeof(1 + '2'))
+//shows string '12' because String '2' converts number 1 to it and because it`s easier to convert Number to String
+console.log(' ' + 1 + 0)
+console.log(typeof(' ' + 1 + 0))
+//' ' converts Number 1 to String and then we have the same result as in first example
+console.log(' ' - 1 + 0)
+console.log(typeof(' ' - 1 + 0))
+//we can`t use '-' with String, so it`s not converting to String
+console.log('2' * 3)
+console.log(typeof('2' * '3'))
+//JS automatically converts numbers in these String variables without our permission 
+console.log(2 + 2 + 'px')
+console.log(typeof(2 + 2 + 'px'))
+//because JS reads code from left to right it is firstly did adding 2 + 2 and then converted Number 4 to String '4px'
+console.log('px' + 2 + 1)
+console.log(typeof('px' + 2 + 1))
+//JS firstly converted Number 2 to 'px2' and then just added to it Number 1 as in first example
+console.log('2' - 1)
+console.log(typeof('2' - 1))
+//JS sees that we are trying to do subtraction and it`s looking if it can convert String '2' to Number, here it can so in the result we have Number 1
+console.log('2px' - 1)
+console.log(typeof('2px' - 1))
+//unlike previous exmaple JS can`t convert String '2px' to Number because it has letters "px" in it, thats why result is NotaNumber
+console.log(null + 1)
+console.log(typeof(null + 1))
+//'null' is a variable often used to add something instead of null, so when we add other variable to it, it disappears 
+console.log(undefined + 1)
+console.log(typeof(undefined + 1))
+//result is NaN because variable undefined shows absence of variable so Number can`t be added to it.
