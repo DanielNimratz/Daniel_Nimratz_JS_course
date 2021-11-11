da7b9a91763465eb33fa2122b9f18ba0eb89308a
Daniel Nimratz <daniel.nimratz@gmail.com>
2021-11-11 15:59:31 +0200

    Lesson#8 Homework/Task 2 added

---
+++ Lesson#8 Homework/Task 2/index.js
@@ -0,0 +1,25 @@
+let num = prompt("Введіть число від 1 до 100")
+
+// console.log(((Math.trunc(5 / 10) * 10) === 1) && (2 <= Math.trunc(5 / 10) <= 4))
+
+// let first_num = 
+// let second_num = 
+
+
+
+if (num == 1) {
+    console.log(num, "стіл");
+} else if (2 <= num && num <= 4) {
+    console.log(num, "столи");
+} else if ((second_num >= 2) && (first_num)) {
+    console.log(num, "столів");
+}
+
+
+
+
+
+//Находження першого числа
+//console.log(num - (Math.trunc(num / 10)*10));
+//Находження другого числа
+//console.log(Math.trunc(num / 10))
