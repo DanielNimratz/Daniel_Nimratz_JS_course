5c8f69a2051155cffbb8bdd72e2b77b9a6b74b7d
Daniel Nimratz <daniel.nimratz@gmail.com>
2021-11-14 17:51:26 +0200

    Task 3 modified

---
+++ Lesson#8 Homework/Extra task/index.js
@@ -0,0 +1,7 @@
+// function randomIntFromInterval(min, max) { // min and max included 
+//     return Math.floor(Math.random() * (max - min + 1) + min)
+// }
+let str = prompt();
+// alert(str);
+let ch = parseInt(str.split(''));
+alert(ch[0] + ch[1] + ch[2]);
