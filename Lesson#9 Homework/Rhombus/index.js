c2a8fa1052586e9e2d74b963041a7b3952765963
Daniel Nimratz <daniel.nimratz@gmail.com>
2021-11-19 15:10:56 +0200

    Triangle 5 and Rhombus added

---
+++ Lesson#9 Homework/Rhombus/index.js
@@ -0,0 +1,20 @@
+for (let i = 0; i < 4; i++) {
+    let result = '';
+    for (let j = 3 - i; j > 0; j--) {
+        result += 'o';
+    }
+    for (let j = i+(i+1); j > 0; j--){
+        result += '*';
+    }
+    console.log(result);
+}
+for (let i = 3; i > 0; i--) {
+    let result = '';
+    for (let j = i; j < 4; j++) {
+        result += 'o';
+    } 
+    for (let j = i+(i-1); j > 0; j--) {
+        result += '*';
+    }
+    console.log(result)
+}
