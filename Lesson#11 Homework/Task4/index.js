b3fdba966bac99a6c458ea29153d5a6f3b686a6c
Daniel Nimratz <daniel.nimratz@gmail.com>
2021-12-02 16:12:47 +0200

    Lesson#11 Homework/Task4 added

---
+++ Lesson#11 Homework/Task4/index.js
@@ -0,0 +1,25 @@
+let a = {
+    b: {
+        c: {
+            d: 1,
+            m: 2
+        }
+    },
+    e: {
+        f: 1
+    },
+    g: 1
+}
+
+function copyObject(obj) {
+    let copy = {};
+    for (key in obj) {
+        if (typeof(obj[key]) === "object") {
+            copy[key] = copyObject(obj[key]);
+        } else {
+            copy[key] = obj[key];
+        }
+    }
+    return copy;
+}
+console.log(copyObject(a));
