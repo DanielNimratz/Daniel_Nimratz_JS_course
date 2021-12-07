ced8eb14f798d1e15b6c04c44df1a3de6123e530
Daniel Nimratz <daniel.nimratz@gmail.com>
2021-12-07 17:22:04 +0200

    Lesson#12 Homework/Task1 modified

--- Lesson#12 Homework/Task1/index.js
+++ Lesson#12 Homework/Task1/index.js
@@ -1,8 +1,8 @@
-arr = [1,2,4,[1,5,4],{key: 'number'},null]
+arr = [1,2,4,[1,5,4],{key: "nummber"},null];
+console.log(arr);
 function reverseByDan(arr) {
-    arr.unshift(arr[arr.length-1])
+    arr.unshift(arr[arr.length-1]);
     arr.pop();
     return arr;
 }
-console.log(reverseByDan(arr))
-
+console.log(reverseByDan(arr));
