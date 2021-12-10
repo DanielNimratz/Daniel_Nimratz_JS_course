3d29976569ac2770deb93bb91e1b5620447113b1
Daniel Nimratz <daniel.nimratz@gmail.com>
2021-12-10 08:56:37 +0200

    Lesson#12 Homework/ExtraTask1 modified

--- Lesson#12 Homework/ExtraTask1/index.js
+++ Lesson#12 Homework/ExtraTask1/index.js
@@ -1,7 +1,13 @@
 const alph = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 
 function continueAlphabet(arr) {
-    let answer = alph[alph.indexOf(arr[0]) + arr.length];
-    return answer;
+    let start = alph.indexOf(arr[0]);
+    let end = alph.indexOf(arr[arr.length - 1]);
+    let copy = alph.slice(start, end + 1);
+    for (i of copy) {
+        if (arr.includes(i) == false) {
+            return i;
+        }
+    }
 }
-console.log(continueAlphabet(['d', 'e', 'f']));
+console.log(continueAlphabet(['a', 'b','c', 'd', 'f', 'g']));
