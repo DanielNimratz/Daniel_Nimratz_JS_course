f21a39595c0070abc9b8369103b4e8945473332f
Daniel Nimratz <daniel.nimratz@gmail.com>
2022-01-26 10:11:06 +0200

    Lesson#16 Homework/Task1 added

---
+++ Lesson#16 Homework/Task1/index.js
@@ -0,0 +1,14 @@
+function getTime() {
+    var date = new Date();
+    var time = prettyNum(date.getHours()) + ":" + prettyNum(date.getMinutes()) + ":" + prettyNum(date.getSeconds());
+    console.log(time);
+}
+function prettyNum(num) {
+    if (num < 10) {
+        return '0' + num;
+    }
+    else {
+        return num;
+    }
+}
+getTime();
---
+++ Lesson#16 Homework/Task1/index.ts
@@ -0,0 +1,14 @@
+function getTime(): void {
+    let date: Date = new Date();
+    let time: string = prettyNum(date.getHours()) + ":" + prettyNum(date.getMinutes()) + ":" + prettyNum(date.getSeconds());
+    console.log(time);
+}
+
+function prettyNum (num: number): string | number {
+    if (num < 10) {
+        return '0'+num;
+    } else {
+        return num;
+    }
+}
+getTime();
